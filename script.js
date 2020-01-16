847fa9115b5b429726575a04b8ff9af162e4653e
Abhinandan Mittal <41774513+abhimittal342@users.noreply.github.com>
2020-01-16 23:51:28 +0530

    contact us order now button added

---
+++ script.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+  // Scroll to the desired section on click
+  // Make sure to add the `data-scroll` attribute to your `<a>` tag.
+  // Example: 
+  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+  function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('href')); 
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 500);
+  }
+  $('[data-scroll]').on('click', scrollToSection);
+}(jQuery));
