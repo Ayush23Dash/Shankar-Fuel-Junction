bc0c322c4a96ec8a4dc29fed8c11c51ec0813892
Ayush23Dash <ayushshanker23@gmail.com>
2020-02-05 01:31:07 +0530

    Added gototop button and gallery navbar

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 jQuery(document).ready(function($) {
   // Scroll to the desired section on click
   // Make sure to add the `data-scroll` attribute to your `<a>` tag.
-  // Example: 
+  // Example:
   // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
   function scrollToSection(event) {
     event.preventDefault();
-    var $section = $($(this).attr('href')); 
+    var $section = $($(this).attr('href'));
     $('html, body').animate({
       scrollTop: $section.offset().top
     }, 500);
   }
   $('[data-scroll]').on('click', scrollToSection);
 }(jQuery));
+
+mybutton = document.getElementById("myBtn");
+window.onscroll = function() {
+  {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+};
+
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval);
+    },15);
+}
